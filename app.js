8ad971ed5231f1cfa9e8fedcfa432b3e7b0864dc
Hafieddz <hasmy41@gmail.com>
2024-03-29 02:21:47 +0800

    feat : add some middleware and configure views engine (ejs)

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const router = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const router = require("./routes");
 
 const app = express();
 
-app.use(express.json())
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname)));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.use(router);
 
