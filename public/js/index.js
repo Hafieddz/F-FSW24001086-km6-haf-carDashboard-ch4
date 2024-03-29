4c49da70dd831bfbea3c4468eb3f454aa283e2a5
Hafieddz <hasmy41@gmail.com>
2024-03-30 07:14:23 +0800

    feat : add js script file and deleting some unused file

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+const inputCapacity = document.getElementById("input-capacity");
+const capacityDiv = document.getElementById("capacity-div")
+const photoDiv = document.getElementById("photo-div");
+const inputPhoto = document.getElementById('input-photo');
+
+
+function setCapacityInput(type) {
+  inputCapacity.value = type;
+  capacityDiv.innerText = type;
+  console.log(inputCapacity.value);
+}
+
+photoDiv.addEventListener('click', () => {
+  inputPhoto.click();
+})
+
+inputPhoto.onchange = function(e) {
+  const file = this.value;
+  const fileName = file.replace(/.*[\/\\]/, '');
+  photoDiv.innerText = fileName;
+}
+
+
+
+
