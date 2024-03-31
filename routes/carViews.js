bc40124cbc207d36df8e8ba34c2105c0f634dc8d
Hafieddz <hasmy41@gmail.com>
2024-03-31 12:58:17 +0800

    feat : create router for deleting data,etc

--- routes/carViews.js
+++ routes/carViews.js
@@ -4,7 +4,9 @@ const carPageController = require('../controllers/carPageController');
 
 router.route('/').get(carPageController.carPage);
 router.route('/add_cars').get(carPageController.createCarsPage);
-router.route('/update').get(carPageController.updateCarsPage);
+router.route('/update/:id').get(carPageController.updateCarPage);
 router.route('/admin/create_car').post(carPageController.createCars);
+router.route('/admin/update_car/:id').post(carPageController.updateCar);
+router.route('/admin/delete_car/:id').post(carPageController.deleteCar);
 
 module.exports = router;
