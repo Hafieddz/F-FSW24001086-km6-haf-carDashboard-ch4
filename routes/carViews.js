00c4fba352e95cee2617634532fc5d6bdd1867cd
Hafieddz <hasmy41@gmail.com>
2024-04-01 12:57:16 +0800

    feat : update router for uploading image

--- routes/carViews.js
+++ routes/carViews.js
@@ -1,12 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const upload = require("../middleware/upload");
 
-const carPageController = require('../controllers/carPageController');
+const carPageController = require("../controllers/carPageController");
 
-router.route('/').get(carPageController.carPage);
-router.route('/add_cars').get(carPageController.createCarsPage);
-router.route('/update/:id').get(carPageController.updateCarPage);
-router.route('/admin/create_car').post(carPageController.createCars);
-router.route('/admin/update_car/:id').post(carPageController.updateCar);
-router.route('/admin/delete_car/:id').post(carPageController.deleteCar);
+router.route("/").get(carPageController.carPage);
+router.route("/add_cars").get(carPageController.createCarsPage);
+router.route("/update/:id").get(carPageController.updateCarPage);
+
+router
+  .route("/admin/create_car")
+  .post(upload.single("carPhoto"), carPageController.createCar);
+router.route("/admin/update_car/:id").post(carPageController.updateCar);
+router.route("/admin/delete_car/:id").post(carPageController.deleteCar);
 
 module.exports = router;
+  
